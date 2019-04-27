17c4b6fdf80a320c8e3ab4e0e21d4ec27fc25455
tbalint19 <toth910719balint@gmail.com>
2019-04-27 15:05:33 +0200

    cleansed main

--- src/main.js
+++ src/main.js
@@ -1,49 +1,40 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-
-import router from './router'
-import store from './store'
-
 import Notifications from 'vue-notification'
 import BootstrapVue from "bootstrap-vue"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon'
-
+import router from './router'
+import store from './store'
 
 // Plugins
 Vue.use(BootstrapVue)
 Vue.use(Notifications)
 Vue.use(VueI18n)
 
-
 // Components
 Vue.component('navbar-component', require('@/components/common/navbar-component').default)
 Vue.component('v-icon', Icon)
 
-
-
-// Config
-import englishDictionary from '../static/dictionary/eng.json'
-import hungarianDictionary from '../static/dictionary/hu.json'
-let messages = {
-  en: englishDictionary,
-  hu: hungarianDictionary
-}
+// Language config
 const i18n = new VueI18n({
   locale: 'en',
-  messages
+  messages: {
+    en: require('../static/dictionary/eng.json'),
+    hu: require('../static/dictionary/hu.json')
+  }
 })
 
+// Api config
 Vue.prototype.$todoApi = require("./api/todo.js").default
 
-
-
-// Init
+// Dev config
 Vue.config.productionTip = false
 Vue.config.devtools = process.env.NODE_ENV === 'development'
 
+// Init
 import App from './App'
 new Vue({
   components: { App },
