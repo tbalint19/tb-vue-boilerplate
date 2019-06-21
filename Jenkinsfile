27ef4f46dd47902c569780fc2b950abde258bf4a
tbalint19 <toth910719balint@gmail.com>
2019-06-21 10:11:24 +0200

    sonar throws valid error

--- Jenkinsfile
+++ Jenkinsfile
@@ -72,13 +72,7 @@ def validateTestCases() {
 
 def sonar(String branch) {
   stage('Static code analysis (sonarqube)') {
-    def jobName = "${env.JOB_NAME}"
-    def localBranch = branch.replaceFirst("origin/", "")
-    def sonarBranchKey = localBranch
-    def project = sonarBranchKey == "master" ? jobName : "${jobName}:${sonarBranchKey}"
-    def sanitizedSonarProjectName = project.replaceAll("/", "-")
     sh 'npm run sonar'
-    // gradlew "sonarqube -Dsonar.projectName=${sanitizedSonarProjectName} -Dsonar.projectKey=${sanitizedSonarProjectName}"
   }
 }
 
--- sonar.js
+++ sonar.js
@@ -1,13 +1,25 @@
 var sonarqubeScanner = require('sonarqube-scanner');
+var request = require('request');
+
+var projectName = "tb-vue-boilerplate"
+var serverUrl = "http://172.17.64.60:9004"
+var resultUrl = serverUrl + "/api/issues/search?id=" + projectName
 
 var sonarJob = function(callback) {
   sonarqubeScanner({
-    serverUrl : "http://172.17.64.60:9004"
+    serverUrl
   }, callback);
 }
 
-var sonarAnalysis = function func(...args) {
-  args.forEach(arg => console.log(arg))
+var sonarAnalysis = function() {
+  request(resultUrl, function (error, response, body) {
+  console.log('error:', error)
+  console.log('statusCode:', response && response.statusCode)
+  console.log('body:', body)
+
+  var issues = body["issues"]
+  if (issues) throw "Has issues!!!!!!!!!!!"
+});
 }
 
 sonarJob(sonarAnalysis)
