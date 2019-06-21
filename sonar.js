e8aee6a6b1e1c648b1ea0345d63244f9700adeaf
tbalint19 <toth910719balint@gmail.com>
2019-06-21 09:01:47 +0200

    playground server sonar

--- sonar.js
+++ sonar.js
@@ -1,3 +1,13 @@
-setTimeout(function log() {
-  throw "error on purpose"
-}, 10000)
+var sonarqubeScanner = require('sonarqube-scanner');
+
+var sonarJob = function(callback) {
+  sonarqubeScanner({
+    serverUrl : "http://172.17.64.60:9004"
+  }, callback);
+}
+
+var sonarAnalysis = function func(...args) {
+  args.forEach(arg => console.log(arg))
+}
+
+sonarJob(sonarAnalysis)
