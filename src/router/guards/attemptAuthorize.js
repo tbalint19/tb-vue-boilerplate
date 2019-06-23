f3fb5b62bb6c1acbf5e5b0051e2e64f1d7d3048d
tbalint19 <toth910719balint@gmail.com>
2019-06-23 20:21:50 +0200

    bug - sonar - attemptAuthenticate;2C

--- src/router/guards/attemptAuthorize.js
+++ src/router/guards/attemptAuthorize.js
@@ -2,7 +2,7 @@ import store from '../../store'
 import { attemptAuthenticate } from './attemptAuthenticate'
 
 export const attemptAuthorize = (cred) => (to, from, next) => {
-  authenticate(to, from, next)
+  attemptAuthenticate(to, from, next)
   if (
     store.state.user.role == cred ||
     store.state.user.permissions.includes(cred)
