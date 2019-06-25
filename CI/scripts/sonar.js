8e57d40e84b00d0c311e39eb98b5b2bb555708d6
tbalint19 <toth910719balint@gmail.com>
2019-06-25 10:59:05 +0200

    should fail beacuse of issue

--- CI/scripts/sonar.js
+++ CI/scripts/sonar.js
@@ -33,10 +33,14 @@ var bugAnalysis = function() {
     var issues = JSON.parse(body)["issues"]
     var relevantIssues = issues.filter(issue => issue.component.startsWith(projectName))
     var openRelevantIssues = relevantIssues.filter(issue => !issue.resolution == 'FIXED' || !issue.status == 'CLOSED')
+
+    console.log("\n------------ Issues -------------------")
     console.log("Fetched: ", bugUrl)
     console.log("Project: ", projectName)
     console.log("Issues: ", openRelevantIssues.length)
     console.log(openRelevantIssues)
+    console.log("---------------------------------------\n")
+    
     if (openRelevantIssues.length > maxIssues)
       throw "Sonar found " + issues.length + " issues (only " + maxIssues + " is acceptable)"
   })
@@ -48,7 +52,14 @@ var duplicationAnalysis = function() {
     var filesWithDuplications = response["components"]
       .map(c => { return { file: c.name, duplicatedLines: c.measures[0].value } })
       .filter(res => res.duplicatedLines > 0)
-    filesWithDuplications.forEach(f=> console.log(f))
+
+    console.log("\n-------- Duplications -----------------")
+    console.log("Fetched: ", duplicationsUrl)
+    console.log("Project: ", projectName)
+    console.log("Duplications: ", filesWithDuplications.length);
+    filesWithDuplications.forEach(f => console.log(f))
+    console.log("---------------------------------------\n")
+
     if (filesWithDuplications.length > 0)
       throw "Sonar found duplications in " + filesWithDuplications.length + " files (only " + maxDuplications + " is acceptable)"
   })
