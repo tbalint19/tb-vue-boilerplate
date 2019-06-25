3d8a63add905b35a37987690e4e07f4f74cc16bd
tbalint19 <toth910719balint@gmail.com>
2019-06-25 08:31:25 +0200

    duplicate removed

--- CI/scripts/coverage.js
+++ CI/scripts/coverage.js
@@ -6,11 +6,11 @@ var coverage = config.coverage
 var checkTresholds = function(report, name) {
   for (var key in report) {
     if (report[key] < coverage[name][key])
-      throw "" + coverage[name][key] + "% not passed for " + key + " at " + name + " tests!"
+      throw "" + coverage[name][key] + "% not passed for " + key + " at " + name + " tests! (" + report[key] + "%)"
   }
 }
 
-var utilReport = asJson('./reports/coverage/store/text-report/coverage.txt')
+var utilReport = asJson('./reports/coverage/util/text-report/coverage.txt')
 var storeReport = asJson('./reports/coverage/store/text-report/coverage.txt')
 
 checkTresholds(utilReport, "util")
