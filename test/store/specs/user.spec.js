ddec0537b303c05823ac913e6cb238680ff9fc47
tbalint19 <toth910719balint@gmail.com>
2019-11-20 15:52:47 +0100

    tests run

--- test/store/specs/user.spec.js
+++ test/store/specs/user.spec.js
@@ -1,9 +1,10 @@
 import { testStore } from '../util/testStoreFactory'
 import { mockAPI, restoreAPI } from '../util/mockAPI'
 import { stubRouter, restoreRouter } from '../util/mockRouter'
+var jwt = require('jsonwebtoken');
 
 describe('User tests', () => {
-  it('should delete user when set with null', async () => {
+  it('should not login for 403', async () => {
     // given
     let store = testStore()
 
@@ -13,11 +14,32 @@ describe('User tests', () => {
     // then
     expect(store.getters['user/email']).to.equal(null)
   })
+
+  it('should login for 200', async () => {
+    // given
+    let store = testStore()
+
+    // when
+    await successfulLogin(store)
+
+    // then
+    expect(store.getters['user/email']).to.equal('a@b.c')
+  })
 })
 
 export const unsuccessfulLogin = async (store) => {
   const api = mockAPI()
   api['service'].onPost('/api/user/login').reply([403])
+
+  await store.dispatch('user/login')
+
+  restoreAPI(api)
+}
+
+export const successfulLogin = async (store) => {
+  const api = mockAPI()
+  var sessionToken = jwt.sign({ email: 'a@b.c', role: 'USER', permissions: [] }, 'shhhhh');
+  api['service'].onPost('/api/user/login').reply(200, { sessionToken })
 
   await store.dispatch('user/login')
 
