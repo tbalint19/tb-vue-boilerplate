49c498a8cfa0bc9fee286e39e50706fb33e88756
tbalint19 <toth910719balint@gmail.com>
2019-11-26 14:43:03 +0100

    image to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <img alt="Vue logo" src="@/../public/img/logo.png" />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="@/../public/img/logo.png" />
     <center>
       <input
         type="text"
