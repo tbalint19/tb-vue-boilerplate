944b648431732192bbf74d46649fe17b1ec4dfdd
Tóth <tothbalint719@gmail.com>
2019-12-07 18:57:29 +0100

    loading mask

--- src/components/loading.vue
+++ src/components/loading.vue
@@ -1,8 +1,23 @@
 <template lang="html">
   <div class="loading">
-    <v-row>
-      <v-col cols="4" offset="4">
-        Loading
+    <v-row
+      class="fill-height"
+      align-content="center"
+      justify="center"
+    >
+      <v-col
+        class="subtitle-1 text-center"
+        cols="12"
+      >
+        Személyre szabjuk ajánlatainkat a számodra...
+      </v-col>
+      <v-col cols="6">
+        <v-progress-linear
+          color="primary lighten-2"
+          indeterminate
+          rounded
+          height="10"
+        ></v-progress-linear>
       </v-col>
     </v-row>
   </div>
